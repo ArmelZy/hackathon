0f64878a3df5d4dccbb53e319467986ea57454ba
armel <bobzydeucet@gmail.com>
2020-02-10 16:35:07 +0100

    Encode user password using BCrypt algorithm with spring security.

--- src/main/java/com/hackathon/challenge/config/SecurityConfig.java
+++ src/main/java/com/hackathon/challenge/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.hackathon.challenge.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/hackathon/challenge/service/AuthService.java
+++ src/main/java/com/hackathon/challenge/service/AuthService.java
@@ -4,6 +4,7 @@ import com.hackathon.challenge.controller.dto.RegisterRequest;
 import com.hackathon.challenge.model.User;
 import com.hackathon.challenge.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,19 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+
+        return passwordEncoder.encode(password);
     }
 }
